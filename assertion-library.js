b424ffcd3a270099062a2d4267718c12c6a9e1fb
Jon Fister <jonfister@gmail.com>
2020-03-21 21:28:10 +1100

    Abstract test assertion to helper function

--- assertion-library.js
+++ assertion-library.js
@@ -5,13 +5,19 @@ let result, expected
 result = sum(4,5)
 expected = 9
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected} \n`)
-}
+expect(result).toBe(expected)
 
 result = subtract(9,5)
 expected = 4
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected} \n`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe: (expected) => {
+      if (actual !== expected){
+        throw new Error(`${result} is not equal to ${expected} \n`)
+      }
+    }
+  }
 }
